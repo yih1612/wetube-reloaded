cc5dc368decac54da26fbb11fff23219691d5fb7
yih1612 <yih1612@naver.com>
2023-01-29 16:43:33 +0900

    3.3 Responses AND Recap

--- src/server.js
+++ src/server.js
@@ -4,9 +4,16 @@ const PORT = 4000;
 
 const app = express();
 
-const handleHome = () => console.log("Somebody is trying to go home.");
+const handleHome = (req, res) => {
+  return res.send("<h1>love u</h1>");
+};
+
+const handleLogin = (req, res) => {
+  return res.send({ message: "Login here." });
+};
 
 app.get("/", handleHome);
+app.get("/login", handleLogin);
 
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
