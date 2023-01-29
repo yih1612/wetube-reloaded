f1fc843484725391164fa56faf82187905c5d1cf
yih1612 <yih1612@naver.com>
2023-01-29 17:50:46 +0900

    3.6 Middlewares

--- src/server.js
+++ src/server.js
@@ -4,16 +4,32 @@ const PORT = 4000;
 
 const app = express();
 
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  }
+  console.log("Allowed, you may continue.");
+  next();
+};
+
 const handleHome = (req, res) => {
   return res.send("<h1>love u</h1>");
 };
 
-const handleLogin = (req, res) => {
-  return res.send({ message: "Login here." });
+const handleProtected = (req, res) => {
+  return res.send("Welcome th the private lounge.");
 };
 
+app.use(logger);
+app.use(privateMiddleware);
 app.get("/", handleHome);
-app.get("/login", handleLogin);
+app.get("/protected", handleProtected);
 
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
