df1d28b552f523b2e1526a6c42bc34f2eeaf9040
yih1612 <yih1612@naver.com>
2023-02-03 10:36:44 +0900

    4.8 URL Parameters

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,14 @@
 export const trending = (req, res) => res.send("Homepage Videos");
-export const see = (req, res) => res.send("See");
-export const edit = (req, res) => res.send("Edit");
+export const see = (req, res) => {
+  res.send(`Watch Video #${req.params.id}`);
+};
+export const edit = (req, res) => {
+  console.log(req.params);
+  return res.send("Edit");
+};
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => res.send("Delete Video");
+export const deleteVideo = (req, res) => {
+  console.log(req.params);
+  return res.send("Delete Video");
+};
