68bfe790af0cc59cfee9a7a57629036f017ebd6f
yih1612 <yih1612@naver.com>
2023-02-07 15:44:55 +0900

    6.2 Edit Video part One

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -31,9 +31,11 @@ export const trending = (req, res) => {
 export const watch = (req, res) => {
   const { id } = req.params; // const id = req.param.id;
   const video = videos[id - 1];
-  return res.render("watch", { pageTitle: `Watching ${video.title}`, video }); // video = video: video
+  return res.render("watch", { pageTitle: `Watching: ${video.title}`, video }); // video = video: video
 };
-export const edit = (req, res) => res.render("edit");
-export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => res.send("Delete Video");
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+};
+export const postEdit = (req, res) => {};
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -2,15 +2,15 @@ import express from "express";
 import {
   upload,
   watch,
-  edit,
+  getEdit,
+  postEdit,
   deleteVideo,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", upload);
 videoRouter.get("/:id(\\d+)", watch);
-videoRouter.get("/:id(\\d+)/edit", edit);
-videoRouter.get("/:id(\\d+)/delete", deleteVideo);
+videoRouter.get("/:id(\\d+)/edit", getEdit);
+videoRouter.post("/:id(\\d+)/edit", postEdit);
 
 export default videoRouter;
