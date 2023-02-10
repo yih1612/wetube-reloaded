e639dd9890df8763d039a6be8707866a4bdcae71
yih1612 <yih1612@naver.com>
2023-02-10 15:07:17 +0900

    6.17 Exceptions and Validation

--- src/models/Video.js
+++ src/models/Video.js
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  createdAt: Date,
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now, required: true },
   hashtags: [{ type: String }],
   meta: {
-    views: Number,
-    rating: Number,
+    views: { type: Number, default: 0, required: true },
+    rating: { type: Number, default: 0, required: true },
   },
 });
 
