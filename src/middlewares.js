ee9abaaa504b7348b4ec755eaa38b5f5d272060c
yih1612 <yih1612@naver.com>
2023-02-21 17:53:41 +0900

    7.12 MongoStore

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,4 +1,5 @@
 export const localsMiddleware = (req, res, next) => {
+  console.log(req.session);
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "Wetube";
   res.locals.loggedInUser = req.session.user;
