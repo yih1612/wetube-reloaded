dd5f2bc18a8060ddc1ee18519be6315a16777a54
yih1612 <yih1612@naver.com>
2023-02-23 16:01:21 +0900

    7.14 Expiration and Secrets

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 mongoose.set("strictQuery", true);
-mongoose.connect("mongodb://127.0.0.1:27017/wetube");
+mongoose.connect(process.env.DB_URL);
 
 // db 성공여부 start
 const db = mongoose.connection;
