5fed4477228982ea3a581327a39fed0cd1861412
yih1612 <yih1612@naver.com>
2023-02-23 17:25:54 +0900

    7.16 Github Login part One

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,5 +1,4 @@
 export const localsMiddleware = (req, res, next) => {
-  console.log(req.session);
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "Wetube";
   res.locals.loggedInUser = req.session.user;
