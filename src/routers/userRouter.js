a15eda34027eb5c12c2762f57df7425f08d8ed6e
yih1612 <yih1612@naver.com>
2023-02-24 11:38:48 +0900

    7.17 Github Login part Two

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,20 @@
 import express from "express";
-import { logout, edit, remove, see } from "../controllers/userController";
+import {
+  logout,
+  edit,
+  remove,
+  see,
+  startGithubLogin,
+  finishGithubLogin,
+} from "../controllers/userController";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
 userRouter.get("/remove", remove);
+userRouter.get("/github/start", startGithubLogin);
+userRouter.get("/github/finish", finishGithubLogin);
 userRouter.get(":id", see);
 
 export default userRouter;
