27ff11eb789d48be4f56103bb4e55e2f5de4b694
yih1612 <yih1612@naver.com>
2023-02-28 13:57:22 +0900

    8.0 Edit Profile GET

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -134,7 +134,12 @@ export const finishGithubLogin = async (req, res) => {
     return res.redirect("/login");
   }
 };
-export const edit = (req, res) => res.send("Edit User");
+export const getEdit = (req, res) => {
+  return res.render("edit-profile", { pageTitle: "Edit Profile" });
+};
+export const postEdit = (req, res) => {
+  return res.render("edit-profile");
+};
 export const logout = (req, res) => {
   req.session.destroy();
   return res.redirect("/");
