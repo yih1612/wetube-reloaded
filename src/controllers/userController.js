44a7a7dcabc59d5b002f1365be538ba085d57d55
yih1612 <yih1612@naver.com>
2023-02-28 18:03:35 +0900

    8.3 Edit Profile POST part Two

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -141,23 +141,50 @@ export const postEdit = async (req, res) => {
   // ES6
   const {
     session: {
-      user: { _id },
+      user: { _id, email: sessionEmail, username: sessionUsername },
     },
     body: { name, email, username, location },
   } = req;
   // 두줄을 간단히
   // const { id } = req.session.user;
   // const { name, email, username, location } = req.body;
-  await User.findByIdAndUpdate(_id, {
-    name,
-    email,
-    username,
-    location,
-  });
-  return res.render("edit-profile");
+  let searchParam = [];
+  if (sessionEmail !== email) {
+    searchParam.push({ email });
+  }
+  if (sessionUsername !== username) {
+    searchParam.push({ username });
+  }
+  // 변경된 값이 있다면 실행되는 if문
+  if (searchParam.length > 0) {
+    // 같은 username OR email을 사용하는 유저가 있는지 확인
+    const foundUser = await User.findOne({ $or: searchParam });
+    if (foundUser && foundUser._id.toString() !== _id) {
+      // 겹치는 유저가 존재하는데, 그게 본인이 아님 (다른 누군가가 사용중)
+      return res.status(400).render("edit-profile", {
+        pageTitle: "Edit Profile",
+        errorMessage: "This username/email is already taken.",
+      });
+    }
+  }
+  // 사용중인 다른 유저가 없는 username AND email이라면, 내껄로 저장 & UPDATE
+  const updatedUser = await User.findByIdAndUpdate(
+    _id,
+    {
+      name,
+      email,
+      username,
+      location,
+    },
+    { new: true }
+  );
+
+  req.session.user = updatedUser;
+  // req.session.message = "success";
+  return res.redirect("/users/edit");
 };
 export const logout = (req, res) => {
   req.session.destroy();
-  return res.redirect("/");
+  return res.redirect("/users/edit");
 };
 export const see = (req, res) => res.send("See User");
