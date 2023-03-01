9095678801633c02247d2b889329b93966750307
yih1612 <yih1612@naver.com>
2023-03-01 10:42:45 +0900

    8.4 Change Password part One

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -187,4 +187,16 @@ export const logout = (req, res) => {
   req.session.destroy();
   return res.redirect("/users/edit");
 };
+
+export const getChangePassword = (req, res) => {
+  if (req.session.user.socialOnly === true) {
+    return res.redirect("/");
+  }
+  return res.render("./users/change-password", {
+    pageTitle: "Change Password",
+  });
+};
+export const postChangePassword = (req, res) => {
+  return res.redirect("/");
+};
 export const see = (req, res) => res.send("See User");
