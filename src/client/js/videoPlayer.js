70787e38744d5b39fc660cfa1653110d3173cf51
yih1612 <yih1612@naver.com>
2023-03-06 17:29:36 +0900

    11.2 Mute and Unmute

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -2,7 +2,7 @@ const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
-const volume = document.getElementById("volume");
+const volumeRange = document.getElementById("volume");
 
 const handlePlayClick = (e) => {
   if (video.paused) {
@@ -10,14 +10,18 @@ const handlePlayClick = (e) => {
   } else {
     video.pause();
   }
+  playBtn.innerText = video.paused ? "Play" : "Pause";
 };
 
-const handlePlay = () => (playBtn.innerText = "Pause");
-const handlePause = () => (playBtn.innerText = "Play");
-
-const handleMute = (e) => {};
+const handleMute = (e) => {
+  if (video.muted) {
+    video.muted = false;
+  } else {
+    video.muted = true;
+  }
+  muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+  volumeRange.value = video.muted ? 0 : 0.5;
+};
 
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
-video.addEventListener("play", handlePlay);
-video.addEventListener("pause", handlePause);
