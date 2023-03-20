8bce786a285069dd54913cdc44029453ea02618f
yih1612 <yih1612@naver.com>
2023-03-20 15:55:11 +0900

    16.1 Comment Models

--- src/models/User.js
+++ src/models/User.js
@@ -9,6 +9,7 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String },
   location: String,
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
   videos: [{ type: mongoose.Schema.Types.ObjectId, ref: "Video" }],
 });
 
