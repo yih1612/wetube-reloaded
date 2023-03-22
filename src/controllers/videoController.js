c30e1d2bb0f99739106cd52b280df7867bdf1c0c
yih1612 <yih1612@naver.com>
2023-03-22 12:08:02 +0900

    16.6 Rendering Comments

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -19,7 +19,8 @@ export const home = async (req, res) => {
 // watch
 export const watch = async (req, res) => {
   const { id } = req.params; // const id = req.param.id;
-  const video = await Video.findById(id).populate("owner");
+  const video = await Video.findById(id).populate("owner").populate("comments");
+  console.log(video);
   if (!video) {
     return res.render("404", { pageTitle: "Video not found." });
   }
@@ -158,5 +159,7 @@ export const createComment = async (req, res) => {
     owner: user._id,
     video: id,
   });
+  video.comments.push(comment._id);
+  video.save();
   return res.sendStatus(201);
 };
