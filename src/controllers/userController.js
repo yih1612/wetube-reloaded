5eb8c006796754475340002de3ce8befa5d2c106
yih1612 <yih1612@naver.com>
2023-03-24 10:44:26 +0900

    17.6 userController(logout fix)

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -184,9 +184,11 @@ export const postEdit = async (req, res) => {
   // req.session.message = "success";
   return res.redirect("/users/edit");
 };
-export const logout = (req, res) => {
-  req.session.destroy();
-  req.flash("info", "Bye Bye");
+export const logout = async (req, res) => {
+  await req.flash("info", "Bye Bye");
+  req.session.loggedIn = false;
+  req.session.user = null;
+  // req.session.destroy();
   return res.redirect("/");
 };
 
